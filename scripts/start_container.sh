b3a8183531df77b217424dbc3c457524936bb332
kalpanas11 <110428461+kalpanas11@users.noreply.github.com>
2024-04-30 20:12:45 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull kalpana111/go-template-simple-form-app
+sudo docker pull kalpana111/go-template-simple-form-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 kalpana111/go-template-simple-form-app
+sudo docker run -d -p 5000:5000 kalpana111/go-template-simple-form-app
